f512feec12ac6a5709eac1ff68d1fc2f75c9043b
Stephen Oravec <81935688+StephenOravec@users.noreply.github.com>
2025-11-10 21:24:02 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use sycamore::prelude::*;
+use sycamore::web::Html;
 
 #[component]
-fn App() -> View {
+fn App<G: Html>() -> View<G> {
     view! {
         h1 { "Hello from Rust + Sycamore + WebAssembly!" }
     }
